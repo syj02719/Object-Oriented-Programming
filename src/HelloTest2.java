ac5bdad49359ab3924c1ce7d9f3ee5a1d46ae687
yujeong <syj02719@naver.com>
2025-10-02 13:31:47 +0900

    Save all current changes

--- src/HelloTest2.java
+++ src/HelloTest2.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
-public class HelloTest2 {
+class Hello{
+    void sayHello(){
+        System.out.println("hello");
+    }
 }
+
+public class HelloTest2{
+    public static void main(String[] args){
+        Hello h1 = new Hello();
+        h1.sayHello();
+        Hello h2 = new Hello();
+        h2.sayHello();
+    }
+}
+
